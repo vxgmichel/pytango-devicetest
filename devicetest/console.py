925b50047e59a4ef045935c3c0d6d459ace8d8c2
Vincent Michel <vincent.michel@maxlab.lu.se>
2015-05-18 10:32:44 +0200

    update command line interface

--- devicetest/console.py
+++ devicetest/console.py
@@ -1,32 +1,23 @@
 """Contain the context to run a device without a database."""
 
 # Imports
-from time import sleep
 from ast import literal_eval
-from PyTango.server import Device
 from importlib import import_module
 from argparse import ArgumentParser
 from devicetest.context import TangoTestContext
 
 
 # Types
+
 def literal_dict(arg):
     return dict(literal_eval(arg))
-    
-def module(arg):
-    return import_module(arg)
 
 
-# Get device 
-def get_device(module):
+def device(path):
     """Get the device class from a given module."""
-    for value in module.__dict__.itervalues():
-        try:
-            if value != Device and issubclass(value, Device):
-                return value
-        except TypeError:
-            pass
-    raise ValueError('No device class in the given module.')
+    module_name, device_name = path.rsplit(".", 1)
+    module = import_module(module_name)
+    return getattr(module, device_name)
 
 
 # Parse command line arguments
@@ -35,32 +26,31 @@ def parse_command_line_args():
     desc = "Run a given device on a given port."
     parser = ArgumentParser(description=desc)
 
-    msg = 'module containing the device.'
-    parser.add_argument('module', metavar='MODULE',
-                        type=module, nargs=1,help=msg)
+    msg = 'The device to run as a python path.'
+    parser.add_argument('device', metavar='DEVICE',
+                        type=device, help=msg)
 
     msg = "The port to use."
     parser.add_argument('--port', metavar='PORT',
-                      type=int, help=msg, default=0)
+                        type=int, help=msg, default=0)
 
     msg = "The debug level."
     parser.add_argument('--debug', metavar='DEBUG',
-                      type=int, help=msg, default=0)
+                        type=int, help=msg, default=0)
 
     msg = "The properties to set as python dict."
     parser.add_argument('--prop', metavar='PROP',
-                      type=literal_dict, help=msg, default='{}')
-
+                        type=literal_dict, help=msg, default='{}')
 
     args = parser.parse_args()
-    device = get_device(args.module[0])
-    return device, args.port, args.prop, args.debug
+    return args.device, args.port, args.prop, args.debug
+
 
 # Main function
 def main():
     device, port, properties, debug = parse_command_line_args()
-    context = TangoTestContext(device, 
-                               properties=properties, 
+    context = TangoTestContext(device,
+                               properties=properties,
                                port=port,
                                debug=debug).start()
     msg = '{0} started on port {1} with properties {2}.'
