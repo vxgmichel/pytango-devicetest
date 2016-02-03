46691ea658df7856c43840e6a229afda7d01ccee
Vincent Michel <vincent.michel@maxlab.lu.se>
2016-02-03 11:44:34 +0100

    Bump version 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name="python-devicetest",
-      version="0.1.1",
+      version="0.1.2",
       description="Resources for Tango device unit testing.",
       packages=['devicetest'],
       install_requires=['unittest2'],
